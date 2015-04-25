f78270a4b70a1b34b350a429d18ba7d72a1437bf
juliancv <julian.canacuan.valenzuela@correounivalle.edu.co>
2015-04-25 12:02:15 -0500

    mario donde andas?

--- src/dao/UsuarioJpaController.java
+++ src/dao/UsuarioJpaController.java
@@ -15,7 +15,7 @@ import javax.persistence.Query;
 import javax.persistence.EntityNotFoundException;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import orm.Usuario;
+//import orm.Usuario;
 
 /**
  *
